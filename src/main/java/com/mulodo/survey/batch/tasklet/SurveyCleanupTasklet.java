2a4971648dc07fc20e5979616e5d9448d4d873cf
Tri Le <thanhtrixx@gmail.com>
2015-04-13 09:35:06 +0700

    Imp cleanup tasklet

---
+++ src/main/java/com/mulodo/survey/batch/tasklet/SurveyCleanupTasklet.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.mulodo.survey.batch.tasklet;
+
+import java.io.File;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+/**
+ * @author TriLe
+ */
+public class SurveyCleanupTasklet implements Tasklet
+{
+
+    private String cleanupDirectory;
+
+    /*
+     * (non-Javadoc)
+     * @see org.springframework.batch.core.step.tasklet.Tasklet#execute(org.
+     * springframework.batch.core.StepContribution,
+     * org.springframework.batch.core.scope.context.ChunkContext)
+     */
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+            throws Exception
+    {
+
+        File folder = new File(cleanupDirectory);
+
+        // Check is folder
+        if (folder.isDirectory()) {
+            // Delete all file in folder
+            for (File file : folder.listFiles()) {
+                file.delete();
+            }
+        } else {
+            throw new IllegalArgumentException("Input cleanup directory invalid");
+        }
+
+        return RepeatStatus.FINISHED;
+    }
+
+    /**
+     * @param cleanupDirectory
+     *            the cleanupDirectory to set
+     */
+    public void setCleanupDirectory(String cleanupDirectory)
+    {
+        this.cleanupDirectory = cleanupDirectory;
+    }
+
+}
