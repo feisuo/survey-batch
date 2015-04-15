c999edd96d22717a9f520aba84c59d4c2d17a800
Tri Le <le.tri@mulodo.com>
2015-04-15 13:53:27 +0700

    Update SQL command

--- survey_create.sql
+++ survey_create.sql
@@ -1,17 +1,17 @@
-drop table if exists employee cascade
+drop table if exists employee cascade;
 
-drop table if exists leaders cascade
+drop table if exists leaders cascade;
 
-drop table if exists managers cascade
+drop table if exists managers cascade;
 
-drop table if exists surveys cascade
+drop table if exists surveys cascade;
 
 create table employee (
     id  serial not null,
     fullname varchar(128) not null,
     team int4 not null,
     primary key (id)
-)
+);
 
 create table leaders (
     id  serial not null,
@@ -19,14 +19,14 @@ create table leaders (
     fullname varchar(128) not null,
     team int4 not null,
     primary key (id)
-)
+);
 
 create table managers (
     id  serial not null,
     email varchar(256) not null,
     fullname varchar(128) not null,
     primary key (id)
-)
+);
 
 create table surveys (
     id  serial not null,
@@ -37,4 +37,4 @@ create table surveys (
     s2 char(1),
     s3 char(1),
     primary key (id)
-)
+);
